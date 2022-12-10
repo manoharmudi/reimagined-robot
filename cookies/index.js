fb43dc879b50074c93ea53a482f3f71814dd8490
manoharmudi <111361404+manoharmudi@users.noreply.github.com>
2022-12-10 11:18:59 +0530

    Add files via upload

---
+++ cookies/index.js
@@ -0,0 +1,45 @@
+// ******* Example - 1 ********
+// window.onload = function(){
+//     if(document.cookie.length != 0){
+//         var nameValueArray = document.cookie.split("=");
+//         document.getElementById("theme").value = nameValueArray[1];
+//         document.bgColor = nameValueArray[1];
+//     }
+// }
+
+// function setColorCookie(){
+//     var selectedValue = document.getElementById("theme").value;
+//     if(selectedValue != "selectColor"){
+//         document.bgColor = selectedValue;
+//         document.cookie = "color="+ selectedValue+";expires = sat, 5 Nov 2022 10:48:00 UTC;";
+//     }
+// }
+
+// ******* Example - 2 ********
+
+// function setCookie() {
+//     var cookieString = "name="+document.getElementById("txtName").value +
+//                         "; email="+document.getElementById("txtEmail").value +
+//                         ";gender="+document.getElementById("txtGender").value;
+//     document.cookie = cookieString;
+// }
+
+// function getCookie() {
+//     alert(document.cookie);
+// }
+// function clearTextBoxes() {
+//     document.getElementById("txtName").value = "";
+//     document.getElementById("txtEmail").value = "";
+//     document.getElementById("txtGender").value = "";
+// }
+// let text = "The rain in SPAIN stays mainly in the plain";
+// console.log(text.match(/ain/gi));
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// const iterator = text.matchAll("Cats");
+// console.log(iterator);
+
+(function(){
+   b = 5;
+}) () ;
+console.log(b);
